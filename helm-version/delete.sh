56b15cb2dfd5994a4701d0d9bc8ca640984218ad
Prince <opokuprince21@gmail.com>
2024-01-21 20:12:59 -0700

    Delete helm-version directory

--- helm-version/delete.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-helm delete $(helm list | grep -e  auth   -e  weather   -e ui   -e mysql -e redis | awk '{print $1}' 
-)
-
-kubectl delete pvc --all
-
-kubectl get all
--- helm-version/deploy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-helm install  mysql  mysql
-helm install redis  redis 
-helm install  auth auth 
-helm install weather weather 
-helm install  ui ui
-
-kubectl get all
